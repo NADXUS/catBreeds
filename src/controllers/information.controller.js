b465200fc3f7c36f1c0721f1870e358397c78b51
Antoni Acevedo <programador9@solucionesstar.com>
2025-01-04 13:30:48 -0500

    Remove console log statements from InformationController and SearchCatsByBreedUseCase

--- src/controllers/information.controller.js
+++ src/controllers/information.controller.js
@@ -6,7 +6,6 @@ export class InformationController {
   async getFromUrlParams(navigation, setqueryParams) {
     try {
       const response = await this.GetCatFromUrlUseCase.execute(navigation);
-      console.log("response", response);
 
       setqueryParams(response);
     } catch (error) {
--- src/useCases/searchCatsByBreed.useCase.js
+++ src/useCases/searchCatsByBreed.useCase.js
@@ -12,7 +12,6 @@ export class SearchCatsByBreedUseCase {
     }
 
     const data = await this.catsRepository.searchByBreed(searchInput);
-    console.log(data);
 
     if (data.length === 0) {
       throw new Error("No data found");
