f07263b99a7da5a896e241a2f0e2460258977a33
Antoni Acevedo <programador9@solucionesstar.com>
2025-01-04 13:27:08 -0500

    Add ImageInformation component for displaying images

---
+++ src/components/ImageInformation.jsx
@@ -0,0 +1,21 @@
+import { Image, View, StyleSheet } from "react-native";
+
+const ImageInformation = ({ image }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: image }} style={styles.image} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
+
+export default ImageInformation;
