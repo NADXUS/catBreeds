34f4ef3c55e427d1ce71f8471947883a148a5f17
Antoni Acevedo <programador9@solucionesstar.com>
2025-01-04 13:27:25 -0500

    Refactor Information component to use InformationController and separate presentation components

--- src/pages/information.jsx
+++ src/pages/information.jsx
@@ -1,59 +1,33 @@
 import { useNavigation } from "@react-navigation/native";
 import { useEffect, useState } from "react";
-import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
+import { View } from "react-native";
+import HeaderInformation from "../components/headerInformation";
+import ImageInformation from "../components/ImageInformation";
+import DescriptionInformation from "../components/descriptionInformation";
+import { InformationController } from "../controllers/information.controller";
+import InformationTemplate from "../templates/information.template";
+
+const $InformationController = new InformationController();
 
 const Information = () => {
   const navigation = useNavigation();
   const [queryParams, setqueryParams] = useState({
-    cat: {
-      image: {
-        url: "",
-      },
+    image: {
+      url: "",
     },
+    description: "",
+    name: "",
   });
 
   useEffect(() => {
-    const params = navigation
-      .getState()
-      .routes.find((route) => route.name === "Information").params;
-    setqueryParams(params);
+    $InformationController.getFromUrlParams(navigation, setqueryParams);
   }, []);
   return (
-    <View style={{ flex: 1, padding: 20, marginTop: 50 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          width: "100%",
-          marginBottom: 20,
-          alignItems: "center",
-          backgroundColor: "lightblue",
-          padding: 10,
-        }}
-      >
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Icon name="arrow-back" size={30} color="#000" />
-        </TouchableOpacity>
-        <Text style={{ fontWeight: "bold", fontSize: 20, color: "black" }}>
-          {queryParams.cat.name}
-        </Text>
-        <View></View>
-      </View>
-      <View>
-        <Image
-          source={{ uri: queryParams.cat.image.url }}
-          style={{ width: "100%", aspectRatio: "1/1" }}
-        />
-      </View>
-      <ScrollView
-        style={{ padding: 10, backgroundColor: "white", marginTop: 20 }}
-      >
-        <Text style={{ fontWeight: "bold", fontSize: 20, color: "black" }}>
-          {queryParams.cat.description}
-        </Text>
-      </ScrollView>
-    </View>
+    <InformationTemplate>
+      <HeaderInformation catName={queryParams.name} navigation={navigation} />
+      <ImageInformation image={queryParams.image.url} />
+      <DescriptionInformation description={queryParams.description} />
+    </InformationTemplate>
   );
 };
 
