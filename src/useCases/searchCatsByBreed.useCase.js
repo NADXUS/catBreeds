196acd71c8be6488f295957fc7ebada372f581a1
Antoni Acevedo <programador9@solucionesstar.com>
2025-01-04 13:27:41 -0500

    Add SearchCatsByBreedUseCase for searching cats by breed with input validation

---
+++ src/useCases/searchCatsByBreed.useCase.js
@@ -0,0 +1,23 @@
+export class SearchCatsByBreedUseCase {
+  constructor({ catsRepository }) {
+    this.catsRepository = catsRepository;
+  }
+
+  async execute(searchInput) {
+    if (searchInput === "") {
+      throw new Error("No search input");
+    }
+    if (searchInput.match(/\d+/g)) {
+      throw new Error("No numbers allowed");
+    }
+
+    const data = await this.catsRepository.searchByBreed(searchInput);
+    console.log(data);
+
+    if (data.length === 0) {
+      throw new Error("No data found");
+    }
+
+    return data;
+  }
+}
