01ab35345789d40ae782cd74a96e0eabd0268aef
Antoni Acevedo <programador9@solucionesstar.com>
2025-01-03 19:27:20 -0500

    Add constants for API URL and API key

---
+++ src/constants/constants.js
@@ -0,0 +1,7 @@
+const constants = {
+  API_URL: "https://api.thecatapi.com/v1",
+  API_KEY:
+    "live_99Qe4Ppj34NdplyLW67xCV7Ds0oSLKGgcWWYnSzMJY9C0QOu0HUR4azYxWkyW2nr",
+};
+
+export default constants;
